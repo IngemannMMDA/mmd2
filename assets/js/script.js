74c63059aa888b53e2df36ac93e6f306752c2fda
Andreas Hoffmann <44086533+Hoffydox@users.noreply.github.com>
2020-06-05 21:33:02 +0200

    Nu er alt JavaScript kun i Js filen
    Mindre clutter i HTML'en

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,11 @@ function loadPage() {
 
 
     getData(); // henter openweathermap.org DATA
-    initMap()
+    initMap();
     // Showcase sidernes map scroll
-
-
+    filterButtons();
+    filterSelection("alle");
+    
 }
 
 
@@ -144,6 +145,50 @@ function initMap() {
       //}
   
   }
+
+  
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("menuOptions");
+  if (c == "alle") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+function filterButtons() {
+  var btnBox = document.getElementById("menu-filter-Id");
+  var btns = btnBox.getElementsByClassName("filter-btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(){
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+}
 
   // window.onscroll = function() {stickyFunction()};
 
